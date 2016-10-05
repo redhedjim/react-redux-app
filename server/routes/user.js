3c52efed2753b541d6cce2ef1033af2da2038cbf
redhedjim <jamesleotucker@gmail.com>
2016-10-04 22:08:15 -0600

    factored out form markup code & clientside validat

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,39 +1,8 @@
 import express from 'express';
-import Validator from 'validator';
-import isEmpty from 'lodash/isEmpty';
+import validateInput from '../shared/validations/signup';
 
 let router = express.Router();
 
-function validateInput(data){
-    let errors = {};
-
-    if(Validator.isEmpty(data.username)) {
-        errors.username = 'A username is required';
-    }
-    if(Validator.isEmail(data.email)) {
-        errors.email = 'A valid email is required';
-    }
-    if(!Validator.isEmail(data.email)){
-        errors.email = "Email is not valid";
-    }
-    if(Validator.isLength(data.password, {min: 3, max: 20})) {
-        errors.password = 'A password is required';
-    }
-    if(Validator.isEmpty(data.passwordConfirmation)) {
-        errors.passwordConfirmation = 'You must confirm your password';
-    }
-    if(!Validator.equals(data.password, data.passwordConfirmation)) {
-        errors.passwordConfirmation = "Passwords must match"
-    }
-    if(Validator.isEmpty(data.timezone)) {
-        errors.timezone = 'Timezone is required';
-    }
-   
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    }
-}
 router.post('/', (req, res) => {
    const { errors, isValid } = validateInput(req.body);
    if(!isValid) {
