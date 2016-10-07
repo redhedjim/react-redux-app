b04ee3be1d5f58aff560ae4a82f1575af95e01da
redhedjim <jamesleotucker@gmail.com>
2016-10-07 14:57:12 -0600

    renders deleteable flashMessage

--- client/components/flash/FlashMessagesList.js
+++ client/components/flash/FlashMessagesList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import FlashMessage from './FlashMessage';
 import { connect } from 'react-redux';
-
+import { deleteFlashMessage } from '../../actions/flashMessages';
 class FlashMessagesList extends React.Component {
     render() {
         const messages = this.props.messages.map(message =>
-            <FlashMessage key={message.id} message={message} />
+            <FlashMessage key={message.id} message={message} deleteFlashMessage={this.props.deleteFlashMessage} />
         );
         return (
             <div>{ messages }</div>
@@ -13,11 +13,12 @@ class FlashMessagesList extends React.Component {
     }
 }
 FlashMessagesList.propTypes = {
-    messages: React.PropTypes.array.isRequired
+    messages: React.PropTypes.array.isRequired,
+    deleteFlashMessage: React.PropTypes.func.isRequired
 }
 function mapStateToProps(state){
     return {
         messages: state.flashMessages
     }
 }
-export default connect(mapStateToProps)(FlashMessagesList);
+export default connect(mapStateToProps, { deleteFlashMessage })(FlashMessagesList);
